bc940491a6a09063bf963d38e09d1527cd6117da
ArthurLopes191 <arthulopesreal@gmail.com>
2024-12-10 23:56:46 -0300

    AS de inteligencia artificial

--- 2024-2/Desenvolvimento de Sistemas Moveis/api_rick_morty/lib/utils/results.dart
+++
@@ -1,8 +0,0 @@
-sealed class Results{}
-
-class ErrorResult extends Results{
-  final String code;
-  ErrorResult({required this.code});
-}
-class SuccessResult extends Results{}
-class LoadingResult extends Results{}

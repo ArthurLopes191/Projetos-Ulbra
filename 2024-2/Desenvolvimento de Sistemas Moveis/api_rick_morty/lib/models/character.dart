1d059f8d66b060c071ebd291cd11d93812739abe
ArthurLopes191 <arthulopesreal@gmail.com>
2024-12-10 23:47:53 -0300

    AS de inteligencia artificial

--- 2024-2/Desenvolvimento de Sistemas Moveis/api_rick_morty/lib/models/character.dart
+++
@@ -1,9 +0,0 @@
-class Character {
-  final String fullName;
-  final String imageUrl;
-  const Character({required this.fullName, required this.imageUrl});
-
-  factory Character.fromJson(Map<String, dynamic> json) {
-    return Character(fullName: json['fullName'], imageUrl: json["imageUrl"]);
-  }
-}
